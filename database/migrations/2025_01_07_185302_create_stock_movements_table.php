fc63d21778cd5fdc0127eceb040d981d45f6b0a6
AlanJose-dev <alanjsdelima@gmail.com>
2025-01-07 16:09:08 -0300

    Adicionando tabela 'stock_movements' e enum, necessário configurar o model

---
+++ database/migrations/2025_01_07_185302_create_stock_movements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_movements', function (Blueprint $table) {
+            $table->id();
+            $table->enum('movement_type', collect(\App\Enums\Enums\Models\StockMovementTypeEnum::cases())
+            ->pluck('value')->toArray());
+            $table->float('quantity');
+            $table->text('observations')->nullable();
+            $table->foreignId('item_id')->constrained('items', 'id')
+            ->noActionOnDelete()->cascadeOnUpdate();
+            $table->foreignId('responsible_id')->constrained('users', 'id')
+            ->noActionOnDelete()->cascadeOnUpdate();
+            $table->foreignId('supplier_id')->nullable()->constrained('suppliers', 'id')
+            ->nullOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_movements');
+    }
+};
