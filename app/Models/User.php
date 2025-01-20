cae6750f81f34dfb6db0efa1358ae8a7f7253971
Alan José <alanjsdelima@gmail.com>
2025-01-20 14:54:56 -0300

    Reimportando trait HasRoles

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+ use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, HasRoles, SoftDeletes, Notifiable;
