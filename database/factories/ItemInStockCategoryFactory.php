deff0e79215258555a8ace10d6c1603d473547f3
Alan José <alanjsdelima@gmail.com>
2025-01-21 14:43:27 -0300

    Adicionando factory de ItemInStockCategory

---
+++ database/factories/ItemInStockCategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ItemInStockCategory>
+ */
+class ItemInStockCategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'description' => fake()->sentence(),
+            'is_active' => fake()->boolean(),
+            'company_id' => Company::factory(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
