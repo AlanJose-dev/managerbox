54ab2444506800dd67c3bd246c9cadff60ffa405
Alan José <alanjsdelima@gmail.com>
2025-01-21 14:46:55 -0300

    Adicionando factory de StockMovement

---
+++ database/factories/StockMovementFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\Models\StockMovementTypeEnum;
+use App\Models\Company;
+use App\Models\ItemInStock;
+use App\Models\Supplier;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\StockMovement>
+ */
+class StockMovementFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'movement_type' => fake()->randomElement(collect(StockMovementTypeEnum::cases())
+            ->pluck('value')->toArray()),
+            'quantity' => fake()->randomFloat(2),
+            'observations' => fake()->text(),
+            'item_in_stock_id' => ItemInStock::factory(),
+            'company_id' => Company::factory(),
+            'user_id' => User::factory(),
+            'supplier_id' => Supplier::factory(),
+        ];
+    }
+}
