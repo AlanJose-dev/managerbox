17a5e0b2137f34c8e1775aa920d4afe86888f307
Alan José <alanjsdelima@gmail.com>
2025-01-21 14:53:28 -0300

    Adicionando factory do Supplier

---
+++ database/factories/SupplierFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Supplier>
+ */
+class SupplierFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'cnpj' => fake()->cnpj(false),
+            'cpf' => fake()->cpf(false),
+            'email' => fake()->unique()->email(),
+            'phone_number' => Str::unformattedPhoneNumber(fake()->phoneNumber()),
+            'is_active' => fake()->boolean(),
+            'company_id' => Company::factory(),
+            'user_id' => User::factory()
+        ];
+    }
+}
